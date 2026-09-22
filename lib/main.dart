ff891dfcb650b9c44a0ae92f628d30ee621fb4e8
william <wrcmendieta@gmail.com>
2026-09-21 19:50:42 -0600

    Agregando ruta inicial - pagina del login

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:farma_di_mobile/screen/home.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:google_fonts/google_fonts.dart';
-=======
-import 'package:farma_di_mobile/widgets/botton_Navigation.dart';
-import 'package:farma_di_mobile/screen/metrics.dart';
-import 'package:farma_di_mobile/widgets/routes.dart';
->>>>>>> 39b85324467656d1441ee36ac07ccc4a10ab0d92
 
 import 'widgets/routes.dart';
 import 'screen/login.dart';
 import 'screen/home.dart';
 
-<<<<<<< HEAD
 void main() => runApp(const FarmaDiApp());
 
 class FarmaDiApp extends StatelessWidget {
@@ -28,48 +21,12 @@ class FarmaDiApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2C3E50)),
         textTheme: GoogleFonts.montserratTextTheme(),
       ),
-      home: const LoginPage(),
+      initialRoute: AppRoutes.login,
       routes: {
         AppRoutes.home: (_) => const HomeScreen(),
+        AppRoutes.login: (_) => const LoginPage(),
         // Las demás rutas se agregan cuando existan sus pantallas.
       },
     );
   }
 }
-=======
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _currenteIndex = 2;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Metrics(),
-      bottomNavigationBar: buildBottomNavigationBar(context, _currenteIndex, (
-        index,
-      ) {
-        setState(() {
-          _currenteIndex = index;
-        });
-      }),
-    );
-  }
-}
->>>>>>> 39b85324467656d1441ee36ac07ccc4a10ab0d92
