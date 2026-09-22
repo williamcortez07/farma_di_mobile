0fd06b11221543c4ff5ff6e4e2e611e1fc31adb1
william <wrcmendieta@gmail.com>
2026-09-21 20:54:35 -0600

    agregando navegacion login -> home, demas vistas...

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:farma_di_mobile/screen/home.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'widgets/routes.dart';
-import 'screen/login.dart';
+import 'screen/catalog.dart';
 import 'screen/home.dart';
+import 'screen/login.dart';
+import 'screen/logs.dart';
+import 'screen/metrics.dart';
+import 'screen/roles.dart';
+import 'screen/settings.dart';
+import 'widgets/routes.dart';
 
 void main() => runApp(const FarmaDiApp());
 
@@ -19,13 +22,16 @@ class FarmaDiApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2C3E50)),
-        textTheme: GoogleFonts.montserratTextTheme(),
       ),
       initialRoute: AppRoutes.login,
       routes: {
         AppRoutes.home: (_) => const HomeScreen(),
         AppRoutes.login: (_) => const LoginPage(),
-        // Las demás rutas se agregan cuando existan sus pantallas.
+        AppRoutes.catalog: (_) => const CatalogScreen(),
+        AppRoutes.metrics: (_) => const Metrics(),
+        AppRoutes.logs: (_) => const LogsScreen(),
+        AppRoutes.roles: (_) => const RolesScreen(),
+        AppRoutes.settings: (_) => const SettingsScreen(),
       },
     );
   }
