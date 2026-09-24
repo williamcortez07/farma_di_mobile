065216bdfc8f6e399911e3e1fcf6c93e15af0790
william <wrcmendieta@gmail.com>
2026-09-24 14:28:22 -0600

    elaboración de metricas a travez de graficos

--- lib/screen/metrics.dart
+++ lib/screen/metrics.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(const MetricsApp());
@@ -56,6 +57,8 @@ class MetricsPage extends StatelessWidget {
 
 // final
 
+// clase original
+//....
 class Metrics extends StatelessWidget {
   const Metrics({super.key});
   @override
@@ -84,7 +87,7 @@ class Metrics extends StatelessWidget {
               const SizedBox(height: 20),
               _buildMetricsGrid(),
               const SizedBox(height: 20),
-              // _buildChartCard(), // comentado por ahora para compilarción, implementarlo abajo
+              _buildChartCard(),
             ],
           ),
         ),
@@ -179,4 +182,159 @@ class MetricCard extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget _buildChartCard() {
+  return Container(
+    width: double.infinity,
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(16),
+      border: Border.all(color: Colors.grey.shade300),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'ventas vs meta mes actual',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+
+        const Text('En cordobas C\$', style: TextStyle(color: Colors.grey)),
+        const SizedBox(height: 20),
+        _buildSalesChart(),
+      ],
+    ),
+  );
+}
+
+Widget _buildSalesChart() {
+  final List<String> months = [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'Jun',
+    'Jul',
+    'Ago',
+  ];
+
+  final List<FlSpot> salesData = const [
+    FlSpot(0, 25000),
+    FlSpot(1, 26000),
+    FlSpot(2, 26500),
+    FlSpot(3, 26800),
+    FlSpot(4, 25000),
+    FlSpot(5, 27000),
+    FlSpot(6, 29000),
+    FlSpot(7, 30000),
+  ];
+
+  final List<FlSpot> golData = const [
+    FlSpot(0, 30000),
+    FlSpot(1, 32000),
+    FlSpot(2, 33000),
+    FlSpot(3, 35000),
+    FlSpot(4, 36000),
+    FlSpot(5, 37000),
+    FlSpot(6, 38000),
+    FlSpot(7, 39000),
+  ];
+
+  return SizedBox(
+    height: 220,
+    child: LineChart(
+      LineChartData(
+        minX: 0,
+        maxX: 7,
+        minY: 0,
+        maxY: 39000,
+        gridData: FlGridData(
+          show: true,
+          drawVerticalLine: false,
+          horizontalInterval: 20000,
+          getDrawingHorizontalLine: (value) {
+            return FlLine(color: Colors.grey.shade200, strokeWidth: 1);
+          },
+        ),
+        borderData: FlBorderData(show: false),
+        titlesData: FlTitlesData(
+          topTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          rightTitles: const AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 20000,
+              reservedSize: 40,
+              getTitlesWidget: (value, meta) {
+                return Text(
+                  '${(value / 1000).toInt()}K',
+                  style: const TextStyle(fontSize: 10, color: Colors.grey),
+                );
+              },
+            ),
+          ),
+
+          // aqui le pasamos a los indeces inferiores los meses definidos en months
+          // enero = 0 -> enero es el indice 0,.. febrero es el indice 1
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 1,
+              reservedSize: 30,
+              getTitlesWidget: (value, meta) {
+                int index = value.toInt();
+                if (index < 0 || index >= months.length) {
+                  return const SizedBox.shrink();
+                }
+                return SideTitleWidget(
+                  meta: meta,
+                  space: 8,
+                  child: Text(
+                    months[index],
+                    style: const TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        lineBarsData: [
+          LineChartBarData(
+            spots: salesData,
+            isCurved: true,
+            color: Colors.blue,
+            barWidth: 3,
+            dotData: const FlDotData(show: false),
+            belowBarData: BarAreaData(
+              show: true,
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue.withValues(alpha: 0.20),
+                  Colors.blue.withValues(alpha: 0),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+
+          LineChartBarData(
+            spots: golData,
+            isCurved: true,
+            color: Colors.green,
+            barWidth: 2,
+            dotData: const FlDotData(show: false),
+          ),
+        ],
+      ),
+    ),
+  );
 }
